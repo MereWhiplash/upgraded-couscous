4f5a63126e560722a8b35d9a45c6c3abbde82e6d
Aaron Shortt <merewhiplash@icloud.com>
2022-01-19 14:27:30 +0000

    Action and spec

---
+++ spec/action_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../Action'
+
+RSpec.describe Action, '#Actions' do
+  let(:name) { 'ActionName' }
+  let(:type) { 'PrintAction' }
+  let(:options) { { 'message' => 'Message Option' } }
+
+  let(:action) { Action.new(name, type, options) }
+
+  context 'with required params' do
+    it 'is valid' do
+      expect(action.valid?).to eq true
+    end
+  end
+
+  context 'without required params' do
+    context 'like name' do
+      let(:name) { nil }
+
+      it 'is not valid' do
+        expect(action.valid?).to eq false
+      end
+    end
+
+    context 'like type' do
+      let(:type) { nil }
+
+      it 'is not valid' do
+        expect(action.valid?).to eq false
+      end
+    end
+
+    context 'like options' do
+      let(:options) { nil }
+
+      it 'is not valid' do
+        expect(action.valid?).to eq false
+      end
+    end
+  end
+end
